f65a0d29da1e5039bdbb45ca4e48463577a0051f
Dkps0081 <deepak.prasad@iitg.ac.in>
2022-10-01 22:24:18 +0530

    deploy setup

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./keys');
+// const keys = require('./keys');
 const User = require('../models/user-model');
 
 passport.serializeUser((user, done) => {
